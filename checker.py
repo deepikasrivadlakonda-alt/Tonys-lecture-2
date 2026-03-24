f2af330e9598287e933aa424ef46588d87c7bc64
deepikasrivadlakonda-alt <deepikasrivadlakonda@gmail.com>
2026-03-24 15:10:56 +0000

    Add working URL checker

--- checker.py
+++ checker.py
@@ -1,25 +1,31 @@
 
 import sys
-
 import requests
 
-if __name__ == '__main__':
+try:
+    print(f'Reading URLs from {sys.argv[1]!r} ...')
 
-    try:
-        print(f'Reading URLs from {sys.argv[1]!r}...')
+    with open(sys.argv[1], 'r') as f:
+        for possible_url in f:
+            try:
+                possible_url = possible_url.strip()
+                r = requests.get(possible_url)
 
-        with open(sys.argv[1], 'r') as f:
-            for possible_url in f:
-                try:
-                    possible_url = possible_url.strip()
-                    r = requests.get(possible_url)
-                    if r.status_code == 200:
-                        print(f'{possible_url} exists.')
-                    else:
-                        print(f'{possible_url} responded, but resource not there.')
-                except:
-                    print(f'{possible_url} did not respond.')
-    except IndexError:
-        print('Missing file name after command.')
-    except FileNotFoundError:
-        print(f'Cannot open {sys.argv[1]!r}')
+                if r.status_code != 200:
+                    print(f'{possible_url!r} responded, but resource not there.')
+
+            except requests.exceptions.SSLError:
+                print(f'{possible_url!r} has an SSL issue.')
+
+            except requests.exceptions.ConnectionError:
+                print(f'{possible_url!r} did not connect.')
+
+            except:
+                print(f'Something unexpected happened with {possible_url!r}.')
+
+except IndexError:
+    print('Missing file name after command.')
+
+except FileNotFoundError:
+    print(f'Cannot open {sys.argv[1]!r}')
+
