10e4faab8c796a08b9b8660093bf8477a642fd62
deepikasrivadlakonda-alt <deepikasrivadlakonda@gmail.com>
2026-03-24 14:29:38 +0000

    Check the links

---
+++ checker.py
@@ -0,0 +1,25 @@
+
+import sys
+
+import requests
+
+if __name__ == '__main__':
+
+    try:
+        print(f'Reading URLs from {sys.argv[1]!r}...')
+
+        with open(sys.argv[1], 'r') as f:
+            for possible_url in f:
+                try:
+                    possible_url = possible_url.strip()
+                    r = requests.get(possible_url)
+                    if r.status_code == 200:
+                        print(f'{possible_url} exists.')
+                    else:
+                        print(f'{possible_url} responded, but resource not there.')
+                except:
+                    print(f'{possible_url} did not respond.')
+    except IndexError:
+        print('Missing file name after command.')
+    except FileNotFoundError:
+        print(f'Cannot open {sys.argv[1]!r}')
